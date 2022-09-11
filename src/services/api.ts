2f84d3844d8b88090ab3666519a4493fc0eb062e
ericpandrade <ericpandrade085@gmail.com>
2022-09-11 15:18:28 -0300

    hotfix: adjust the url to json-server

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,5 @@
 import Axios from "axios";
 
 export default Axios.create({
-  baseURL: "https://my-json-server.typicode.com/ericpandrade/Invoice-Generator",
+  baseURL: "http://localhost:3333",
 });
